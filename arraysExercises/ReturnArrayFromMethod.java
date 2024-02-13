5ea45c8033191b75d30d2bae05e33318ac808eb4
Marin Tenzherski <marin_mm@abv.bg>
2024-02-13 21:57:48 +0200

    New lessons 2

--- arraysExercises/ReturnArrayFromMethod.java
+++ arraysExercises/ReturnArrayFromMethod.java
@@ -1,8 +1,16 @@
 package arraysExercises;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ReturnArrayFromMethod {
 
     public static void main(String[] args) {
+
+
+
+        System.out.println("Array is this");
+        System.out.println(Arrays.toString(readArray()));
 
     }
 
