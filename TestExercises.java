546d867b928c84143f98d8bde42fe5a7b830f133
MarinVT <marin_mm@abv.bg>
2025-05-20 23:00:36 +0300

    New approach

--- TestExercises.java
+++ TestExercises.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class TestExercises {
 
@@ -9,75 +7,27 @@ public class TestExercises {
     public static void main(String[] args) {
 
 
-    Integer[] intArray = new Integer[] {1,2,3,4};
-    String [] stringsArray = new String[] {"VW", "tt", "bmw", "volga"};
-    String str = "marin igrata NOMER 1";
-    char[] strToChars = str.toCharArray();
+    int[][] intArray = new int[10][10];
+    String [] stringsArray = new String[] {"WV", "tt", "bmw", "volga"};
 
-    int[] arr = {1, 12, 3, 4};
+    int[] arr = {1,2,3,4};
 
-    int max = Arrays.stream(arr).max().getAsInt();
-    System.out.println(max);
-
-    int min = Arrays.stream(arr).min().getAsInt();
-        System.out.println(min);
+        System.out.println(findIndexArr(arr, 1));
 
     }
 
-    public static <T> boolean arrContainsElement(final T[] arr, final T v) {
+    static int findIndexArr(int[] arr, int indexTarget) {
 
-        for (final T e : arr) {
-            if (e == v || v != null && v.equals(e)) {
-                return true;
-            }
-        }
+        int len = arr.length;
 
-        return false;
-    }
-
-
-    static void addElementAtPositionInArray(Integer arr[], int element, int position) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(arr));
-        System.out.println("The original array is: " + Arrays.toString(arr));
-
-        list.add(position - 1, element);
-
-        arr = list.toArray(arr);
-
-        System.out.println("The new array with added element : " + element + " in position "
-                + position
-                + " is: "
-                + Arrays.toString(arr));
+        return IntStream.range(0, len)
+                .filter(e -> indexTarget == arr[e])
+                .findFirst()
+                .orElse(-1);
 
     }
 
-    public static int maxNumArr(int[] arr) {
-        int max = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-
-        }
-
-        return max;
-    }
-
-    public static int minNumArr(int[] arr) {
-        int min = arr[0];
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-
-        }
-
-        return min;
-    }
 
 }
 
--- arraysExercises/FindIndexArrElement.java
+++ arraysExercises/FindIndexArrElement.java
@@ -1,9 +1,16 @@
 package arraysExercises;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class FindIndexArrElement {
     public static void main(String[] args) {
         int[] myArr = {1,2,3, 4, 5, 6};
-        System.out.println(findIndex(myArr, 1));
+        int t = 7;
+
+
+        System.out.println(findIndexArrList(myArr, 3));
+
     }
 
     public static int findIndex(int[] arr1, int itemIndexPosition) {
@@ -22,5 +29,31 @@ public class FindIndexArrElement {
             }
         }
         return -1;
+    }
+
+    /* Using ArrayList - part of collection framework. It provides dynamic sized arrays in java.I have no need
+       to mention the size of the array when I have to create it. It is slower.
+    */
+    public static int findIndexArrList(int arr[], int indexFind) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i : arr) {
+            list.add(i);
+        }
+        System.out.println("The index of the number in the array is:");
+        return list.indexOf(indexFind);
+    }
+
+
+    static int findIndexArr(int[] arr, int indexTarget) {
+
+        int len = arr.length;
+
+        return IntStream.range(0, len)
+                .filter(e -> indexTarget == arr[e])
+                .findFirst()
+                .orElse(-1);
+
     }
 }
